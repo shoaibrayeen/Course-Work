6827270e66d411d8368ede4568724d83a2492ffc
MOHD SHOAIB RAYEEN <shoaib.mca17.du@gmail.com>
2017-12-25 18:39:07 +0530

    worst case time complexity : O(R*C)

---
+++ Complexity Analysis/Recursive/q1.cpp
@@ -0,0 +1,17 @@
+//What is the worst case time complexity of the following code:
+//worst case time complexity : O(R*C)
+
+int memo[101][101];
+int findMinPath(vector<vector<int> >& V, int r, int c) {
+  int R = V.size();
+  int C = V[0].size();
+  if (r >= R || c >= C) return 100000000; // Infinity
+  if (r == R - 1 && c == C - 1) return 0;
+  if (memo[r][c] != -1) return memo[r][c];
+  memo[r][c] =  V[r][c] + min(findMinPath(V, r + 1, c), findMinPath(V, r, c + 1));
+  return memo[r][c];
+}
+
+Callsite : 
+memset(memo, -1, sizeof(memo));
+findMinPath(V, 0, 0);
