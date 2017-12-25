598c3c8bc02d853ffb685e2cfcc1f9a76094617d
MOHD SHOAIB RAYEEN <shoaib.mca17.du@gmail.com>
2017-12-25 18:34:49 +0530

    log n time

---
+++ Complexity Analysis/Iterative/q1.cpp
@@ -0,0 +1,14 @@
+//In the following C++ function, let n >= m.
+//  Θ(logn)
+
+int main() {
+    int gcd(int n, int m) {
+            if (n%m ==0) return m;
+            if (n < m) swap(n, m);
+            while (m > 0) {
+                n = n%m;
+                swap(n, m);
+            }
+            return n;
+    }
+}
