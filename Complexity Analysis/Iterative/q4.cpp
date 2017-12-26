c545e431d7f422e16f2b19fb7849ba7e908bab22
MOHD SHOAIB RAYEEN <shoaib.mca17.du@gmail.com>
2017-12-26 14:27:54 +0530

    O(N + M) time , O(1) space

---
+++ Complexity Analysis/Iterative/q4.cpp
@@ -0,0 +1,13 @@
+//What is the time, space complexity of following code :
+// Ans :  O(N + M) time , O(1) space
+int main() {
+
+        int a = 0, b = 0;    
+        for (i = 0; i < N; i++) {
+            a = a + rand();  
+        }
+        for (j = 0; j < M; j++) {
+            b = b + rand();
+        }
+        return 0;
+}
