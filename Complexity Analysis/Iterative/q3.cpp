227ccbc601e9f4ee4f9f50c74f539c0d40be5d75
MOHD SHOAIB RAYEEN <shoaib.mca17.du@gmail.com>
2017-12-26 14:27:23 +0530

    time complexity - O(log N)

---
+++ Complexity Analysis/Iterative/q3.cpp
@@ -0,0 +1,11 @@
+//What is the time complexity of the following code :
+// Ans :  time complexity - O(log N)
+
+int main() {
+
+        int a = 0, i = N;
+        while (i > 0) {
+            a += i;
+            i /= 2;
+        }
+}
