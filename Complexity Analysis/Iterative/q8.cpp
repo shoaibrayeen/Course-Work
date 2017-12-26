02995603b23bef667c4e306ce21a00f39c740d2e
MOHD SHOAIB RAYEEN <shoaib.mca17.du@gmail.com>
2017-12-26 14:30:29 +0530

    time complexity: O(N*N)

---
+++ Complexity Analysis/Iterative/q8.cpp
@@ -0,0 +1,12 @@
+
+//What is the time complexity of the following code :
+// time complexity: O(N*N)
+int main () {
+    int a = 0;
+    for (i = 0; i < N; i++) {
+        for (j = N; j > i; j--) {
+            a = a + i + j;
+        }
+    }
+
+}
