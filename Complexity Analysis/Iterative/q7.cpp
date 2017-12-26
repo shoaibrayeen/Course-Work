e36d5fd65586a79f9ac04c83dcefe62493d68ac5
MOHD SHOAIB RAYEEN <shoaib.mca17.du@gmail.com>
2017-12-26 14:29:53 +0530

    O(N * N) time, O(1) space

---
+++ Complexity Analysis/Iterative/q7.cpp
@@ -0,0 +1,14 @@
+//What is the time, space complexity of following code :
+//O(N * N) time, O(1) space
+
+int main() {
+    int a = 0, b = 0;    
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            a = a + j;
+        }
+    }
+    for (k = 0; k < N; k++) {
+        b = b + k;
+    } 
+}
