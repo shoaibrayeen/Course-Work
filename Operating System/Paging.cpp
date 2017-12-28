1aaa67040c87a6a1cb705b986de42badcd67f8c5
MOHD SHOAIB RAYEEN <shoaib.mca17.du@gmail.com>
2017-12-29 02:17:25 +0530

    Paging

---
+++ Operating System/Paging.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int search(int n,int *pid,int p)
+{
+	for(int i=0;i<n;i++)
+	{
+		if(p==pid[i])
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+using namespace std;
+int main()
+{
+	int ps,ms;
+	system("clear");
+	cout<<"\nEnter Memory Size\t:\t";
+	cin>>ms;
+	cout<<"\nEnter Page Size\t\t:\t";
+	cin>>ps;
+	int ff=ms/ps;
+	int pid[ff],vi[ff];
+	int n=ff;
+	for(int i=0;i<n;i++)
+	{
+		pid[i]=vi[i]=0;
+	}
+	cout<<"\n";
+	int c,psize,rp,k,j,p;
+	while(1)
+	{
+		cout<<"\nFree Frames\t\t:\t"<<ff;
+		cout<<"\n\n\t*****************Frame Table***********************\n";
+		cout<<"\n\nFrame No\tProcess ID\tValid/Invalid\n";
+		for(int i=0;i<n;i++)
+		{
+			cout<<i<<"\t\t   "<<pid[i]<<"\t\t  "<<vi[i]<<"\n";
+		}
+		cout<<"\n1.Allocate\n2.Deallocate\n3.Exit\n";
+		cin>>c; 
+		switch(c)
+		{
+			case 1: cout<<"\nEnter The Size of Process\t:\t";	
+				cin>>psize;
+				k=0;
+				if(psize%ps!=0) k=1;
+				rp=psize/ps +k;
+				if(rp>ff){cout<<"\nRequired Memory Not Available\n";break;}
+				cout<<"\nEnter the process id\t:\t";
+				cin>>p;
+				if(search(n,pid,p)!=0)
+				{
+					j=1;
+					for(int i=0;i<n && j <= rp;i++)
+					{
+						if(vi[i]==0)
+						{
+							pid[i]=p;
+							vi[i]=1;
+							j++;
+						}
+					}
+					ff-=rp;
+					cout<<"\nFrame Successfully Allocated!!!!!!!!!!!\n";
+				}
+				else{cout<<"\nDuplication Not Allowed\n";}
+				break;
+			case 2: cout<<"\nEnter the Process Id For Deallocate the memory\t:\t";
+				cin>>p;
+				if(search(n,pid,p)==0)
+				{
+					for(int i=0;i<n;i++)
+					{
+						if(pid[i]==p)
+						{
+							vi[i]=0;
+							ff++;
+						}
+					}
+					cout<<"\nFrame Successfully Deallocated!!!!!!!!!!!!!!!!!!\n";
+				}
+				else
+				{
+					cout<<"\nInvalid Process Id\n";
+				}
+				break;
+			case 3: exit(0);
+				
+				
+		}
+	}
+	return 0;
+}
