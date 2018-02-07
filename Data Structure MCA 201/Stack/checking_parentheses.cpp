7ef5f06d504cdfbeb5545652e874596dd031ed6e
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-02-07 22:33:54 +0530

    Checking Parentheses using STL

---
+++ Data Structure MCA 201/Stack/checking_parentheses.cpp
@@ -0,0 +1,46 @@
+//
+//  checking_parentheses.cpp
+//  Data Structure
+//
+//  Created by Mohd Shoaib Rayeen on 07/02/18.
+//  Copyright © 2018 Shoaib Rayeen. All rights reserved.
+//
+
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+
+int main() {
+    /*
+     objective : to check parentheses
+     return value : none
+     approach : using stack
+     */
+    
+    stack<char> object;
+    string str;
+    cout << "\nEnter Parentheses\t:\t";
+    cin >> str;
+    int length=str.length();
+    for ( int i=0 ; i < length ; ++i) {
+        if(str[i]=='(') {
+            object.push(str[i]);
+        }
+        else if (str[i]==')') {
+            if(object.empty()) {
+                cout << "\nParentheses are not balanced. \n";
+                goto exit;
+            }
+            object.pop();
+        }
+    }
+    if(object.empty()) {
+        cout << "\nParentheses are balanced. \n";
+    }
+    else {
+        cout << "\nParentheses are not balanced. \n";
+    }
+exit:
+    return 0;
+}
