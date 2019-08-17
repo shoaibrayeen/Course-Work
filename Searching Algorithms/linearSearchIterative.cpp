030c986734c363bc290929a8ec1cd9d15a3d40a5
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-08-18 00:33:17 +0530

    Linear Search Iterative - using Class

--- Searching Algorithms/linearSearchIterative.cpp
+++ Searching Algorithms/linearSearchIterative.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 using namespace std ;
 
-class sort {
+class search {
 	private :
-		int array[10] , number ;
+		int array[100] , number ;
 	public :
-		sort() ;
+		search() ;
 		void input() ;
 		void display() ; 
-		void linearSearch(int) ;
+		int linearSearch(int) ;
 };
 
-sort :: sort () {
-	for ( int i = 0 ; i < 10 ; ++i )
+search :: search () {
+	for ( int i = 0 ; i < 100 ; ++i ) {
 		array[i] = 0;
+	}
 	number = 0 ;
 }
 
-void sort :: input() {
+void search :: input() {
 	cout << "\nEnter number of elemnets\t:\t" ;
 	cin >> number ;
 	cout << "\nEnter Elements\n" ;
@@ -28,18 +29,16 @@ void sort :: input() {
 	}
 }
 
-void sort :: linearSearch(int element) {
+int search :: linearSearch(int element) {
 	for ( int i = 0 ; i < number ; ++i ) {
 		if ( element == array[i] ) {
-			cout << "\nElement " << element << " found at " << ( i + 1 ) << " position \n" ;
-			return ;
+			return i + 1;
 		}
 	}
-	cout << "\nElement " << element << " is not found " ;
-	cout << endl ;
+	return -1;
 }
 
-void sort :: display() {
+void search :: display() {
 	for ( int i = 0 ; i < number ; ++i ) {
 		cout << array[i] <<"\t" ;
 	}
@@ -47,13 +46,19 @@ void sort :: display() {
 }
 
 int main() {
-	sort object;
+	search object;
 	object.input() ;
 	cout << "\nEntered Elements\t:\t" ;
 	object.display() ;
 	int element ;
 	cout << "\nEnter the elemnt which you want to search\t:\t" ;
 	cin >> element ;
-	object.linearSearch(element) ;
+	int res = object.linearSearch( element ) ;
+	if ( res == -1 ) {
+		cout << "\nElement " << element << " is not found.\n " ;
+	}
+	else {
+		cout << "\nElement " << element << " found at " << res << " position.\n" ;
+	}
 	return 0;
 }
