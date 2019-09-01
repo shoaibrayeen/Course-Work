3210f169314778e7504808a33fc9fe47084e1a73
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-09-01 16:10:42 +0530

    Nested Fork - Child creates another process.

--- Operating System/nested_fork.c
+++ Operating System/nested_fork.c
@@ -1,24 +1,20 @@
 #include<stdio.h>
-int main()
-{
+
+int main() {
 	system("clear");
-	int pid=fork();
-	int pid1,i,s;
-	if(pid==0)
-	{
-		int pid1=fork();
-		if(pid1==0)
-		{
+	int pid = fork();
+	int pid1 , s;
+	if(pid == 0) {
+		int pid1 = fork();
+		if(pid1 == 0) {
 			printf("\nGrandchild Id\t:\t%d\nChild Id\t:\t%d\n",getpid(),getppid());
 		}
-		else
-		{
+		else {
 			wait((void*)&s);
 			printf("\nChild Id\t:\t%d\nParent Id\t:\t%d\n",getpid(),getppid());
 		}
 	}
-	else
-	{
+	else {
 		wait((void*)&s);
 		printf("\nParent Id\t:\t%d\nGrandParent Id\t:\t%d\n",getpid(),getppid());
 	}
