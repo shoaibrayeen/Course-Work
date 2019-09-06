03502de0c15ea9b58eb41ac4af3852fc4e1c2517
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-09-07 02:29:33 +0530

    Stable Count Sort

--- Algorithm/stableCountSort.cpp
+++ Algorithm/stableCountSort.cpp
@@ -11,27 +11,29 @@
 
 using namespace std;
 
-int main()
-{
-    int n,i,j;
+int main() {
+    int n;
     printf("Enter size of array ");
     scanf("%d",&n);
     int input[n],output[n];
     printf("Enter elements of array\n");
-    for(i=0;i<n;i++)
+    for(int i = 0; i < n; i++) {
         scanf("%d",&input[i]);
+    }
     stack<int> count[10];
-    for(i=n-1;i>=0;i--)
+    for(int i = n - 1; i >= 0; i--) {
         count[input[i]].push(i);
-    for(i=0,j=0;i<10;i++)
-        while(!count[i].empty())
-        {
-            output[j++]=input[count[i].top()];
+    }
+    for(int i = 0, j = 0; i < 10; i++ ) {
+        while(!count[i].empty()) {
+            output[j++] = input[count[i].top()];
             count[i].pop();
         }
+    }
     printf("Sorted array:- \n");
-    for(i=0;i<n;i++)
+    for(int i = 0; i < n; i++ ) {
         printf("%d ",output[i]);
+    }
     cout << endl;
     return 0;
 }
