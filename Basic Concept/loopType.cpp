7147b6c16b879f5a1eeb939498aaac81ce5f3d94
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-09-11 11:41:45 +0530

    Different Type of Loops and Go to statement!

--- Basic Concept/loopType.cpp
+++ Basic Concept/loopType.cpp
@@ -1,33 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+	// For loop
 	int i ;
 	int endCount = 5 ; 
 	for ( i = 0 ; i < endCount ; ++i ) {
-		cout << "\nFor Loop :: i = " << i+1 ;
+		cout << "\nFor Loop :: i = " << i + 1 ;
 	}
 	cout << endl ;
 
 	goto doWhile ;
-
+	
+	
+	// While
 	whileLoop : i = 0 ;
 	cout << endl ;
 	while ( i < endCount ){
-		cout << "\nWhile Loop :: i = " << i+1 ; 
+		cout << "\nWhile Loop :: i = " << i + 1 ; 
 		++i;
 	}	
 	cout << endl ;
 	
 	goto exit ;
 
+	// Do While
 	doWhile : i = 0 ;
 
 	do{
-		cout << "\nDo While Loop :: i = " << i+1 ;
+		cout << "\nDo While Loop :: i = " << i + 1 ;
 		++i;
 		
-	}while ( i < endCount );
+	} while ( i < endCount );
+	
 	goto whileLoop ;
+	
 	exit : return 0;	
 }
